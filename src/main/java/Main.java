069a6ad1abd9630c7e9c54c614820726e8f3cba6
sik2 <bickwang123@gmail.com>
2023-10-23 09:42:28 +0900

    등록1단계

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,21 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("명령어) ");
-        String command = sc.nextLine();
-        System.out.printf("입력된 명령어 %s\n", command);
+        while (true) {
+            System.out.printf("명령어) ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                System.out.println("시스템을 종료합니다.");
+                break;
+            } else if (command.equals("등록")) {
+                System.out.printf("제목: ");
+                String title = sc.nextLine();
+                System.out.printf("내용: ");
+                String content = sc.nextLine();
+            }
+        }
+
 
         sc.close();
         System.out.println("== 프로그램 끝 ==");
