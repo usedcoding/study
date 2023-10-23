7c643bf24f58e2e5c081548795d438659fe71064
sik2 <bickwang123@gmail.com>
2023-10-23 09:47:57 +0900

    등록2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
 
+        long lastId = 0;
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -14,13 +15,15 @@ public class Main {
                 System.out.println("시스템을 종료합니다.");
                 break;
             } else if (command.equals("등록")) {
+                lastId++;
                 System.out.printf("제목: ");
                 String title = sc.nextLine();
                 System.out.printf("내용: ");
                 String content = sc.nextLine();
+
+                System.out.println(lastId + "번 게시글이 등록되었습니다.");
             }
         }
-
 
         sc.close();
         System.out.println("== 프로그램 끝 ==");
